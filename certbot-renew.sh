d6b96fa52af380eb96c494ad2c4af85683675077
nbabcock <nathan.r.babcock@gmail.com>
2023-07-29 15:13:20 -0500

    chore: remove obsolete docker, nginx, and certbot shell scripts

--- certbot-renew.sh
+++
@@ -1,5 +0,0 @@
-sudo \
-  --preserve-env=AWS_ACCESS_KEY_ID \
-  --preserve-env=AWS_SECRET_ACCESS_KEY \
-  certbot renew
-docker restart nginx
--- certbot.sh
+++
@@ -1,7 +0,0 @@
-sudo \
-  --preserve-env=AWS_ACCESS_KEY_ID \
-  --preserve-env=AWS_SECRET_ACCESS_KEY \
-  certbot certonly \
-  --dns-route53 \
-  -d hypetrigger.io \
-  -d www.hypetrigger.io
